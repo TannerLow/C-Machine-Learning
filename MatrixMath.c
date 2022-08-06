a970df8a3a2e051d272a03aa999b89a9c2575857
Tanner Lowthorp <lowthorpt@gmail.com>
2022-08-05 21:07:47 -0700

    matrix subtraction + mult by transpose

--- MatrixMath.c
+++ MatrixMath.c
@@ -1,11 +1,15 @@
 #include "MatrixMath.h"
+#include <assert.h>
 
 
 bool dotProduct(Matrix* a, Matrix* b, Matrix* result) {
+    assert(a != NULL && b != NULL);
+    assert(a->rowSize == b->columnSize);
+
     // input validation
     if (a == NULL || b == NULL || result == NULL) return false; // invalid operands 
 
-    if (a->columnSize != b->rowSize) return false; // invalid dimensions
+    if (a->rowSize != b->columnSize) return false; // invalid dimensions
 
     Dimensions2D expectedDimensions = { a->columnSize, b->rowSize };
     Dimensions2D actualDimensions = getDimensions(result);
@@ -31,7 +35,35 @@ bool dotProduct(Matrix* a, Matrix* b, Matrix* result) {
 }
 
 bool dotProductTransposeB(Matrix* a, Matrix* b, Matrix* result) {
-    return false;
+    assert(a != NULL && b != NULL);
+    assert(a->rowSize == b->rowSize);
+
+    // input validation
+    if (a == NULL || b == NULL || result == NULL) return false; // invalid operands 
+
+    if (a->rowSize != b->rowSize) return false; // invalid dimensions
+
+    Dimensions2D expectedDimensions = { a->columnSize, b->columnSize };
+    Dimensions2D actualDimensions = getDimensions(result);
+    if (!areEqualSizes(actualDimensions, expectedDimensions)) {
+        return false; // result dimensions unexpected
+    }
+
+    // actual math
+    for (size_t aRow = 0; aRow < a->columnSize; aRow++) {
+        for(size_t bRow = 0; bRow < b->columnSize; bRow++) {
+            // dimensions must align so we can iterate with only 1 variable
+            double sum = 0;
+            for (size_t col = 0; col < a->rowSize; col++) {
+                sum += getMatrixElement(a, aRow, col) * getMatrixElement(b, bRow, col);
+            }
+            if(!setMatrixElement(result, aRow, bRow, sum)) {
+                return false; // attempt to index out of bounds
+            }
+        }
+    }
+
+    return true;
 }
 
 bool elementWiseMultiplication(Matrix* a, Matrix* b, Matrix* result) {
@@ -89,7 +121,30 @@ bool matrixAddition(Matrix* a, Matrix* b, Matrix* result) {
 }
 
 bool matrixSubtraction(Matrix* a, Matrix* b, Matrix* result) {
-    return false;
+    // input validation
+    if (a == NULL || b == NULL || result == NULL) return false; // invalid operands 
+
+    if (!areEqualSizes(getDimensions(a), getDimensions(b))) return false; // invalid dimensions
+
+    Dimensions2D expectedDimensions = getDimensions(a);
+    Dimensions2D actualDimensions = getDimensions(result);
+    if (!areEqualSizes(actualDimensions, expectedDimensions)) {
+        return false; // result dimensions unexpected
+    }
+
+    // actual math
+    for (size_t row = 0; row < a->columnSize; row++) {
+        for(size_t col = 0; col < a->rowSize; col++) {
+
+            double value = getMatrixElement(a, row, col) - getMatrixElement(b, row, col);
+
+            if(!setMatrixElement(result, row, col, value)) {
+                return false; // attempt to index out of bounds
+            }
+        }
+    }
+
+    return true;
 }
 
 bool scaleMatrix(Matrix* a, double scalar, Matrix* result) {
